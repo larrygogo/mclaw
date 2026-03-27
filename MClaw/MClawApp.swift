b67ba8e69ece997650853e70fee31763a0f180b1
larrygogo <351220018@qq.com>
2026-03-27 23:34:21 +0800

    Initial Commit

---
+++ MClaw/MClawApp.swift
@@ -0,0 +1,17 @@
+//
+//  MClawApp.swift
+//  MClaw
+//
+//  Created by 郑靖 on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct MClawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
